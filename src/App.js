b59c7c22a2b99203375ce0f2d4f5a4d508618f6c
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-05-16 12:04:24 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <footer className="premium-footer p-3 p-md-5">
   <h1 className="banner-heading mb-5">About Our Developer</h1> <hr/>
   <p className='lead'>Website Created and maintained by Bhaskar Antony. Enhancing your online experience and services.</p>
-  <p className='lead'><span className='g-text'>Contact: </span> +91 9606729320 | <span className='g-text'>Emai: </span>: bhaskarbabu6@gmail.com</p>
+  <p className='lead'><span className='g-text'>Contact: </span> +91 9606729320 | <span className='g-text'>Email: </span>: bhaskarbabu6@gmail.com</p>
   <p className='lead mb-4'>We're your go-lto destination for website development and maintenance services. Whether you're a small business, a startup, or an individual, we've got the expertise to craft stunning websites that drive results. From responsive design to SEO optimization and social media integration, we offer affordable, custom solutions tailored to your needs. With personalized support and dedication to your satisfaction, we're here to help you elevate your online presence.</p>
   <a href='tel:+919606729320' className="my-btn fs-5">Click for Your Own Website</a>
 </footer>
